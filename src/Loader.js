7abe7713fee5a0983c7f6e608a4f11cc0e03139e
Gustavo Pires <gpiresnt@gmail.com>
2020-12-04 01:09:12 +0000

    fix body html

--- src/Loader.js
+++ src/Loader.js
@@ -2,8 +2,8 @@
 
 function loader(render) {
     document.addEventListener('DOMContentLoaded', () => {
-        const data = JSON.parse(document.body.innerHTML.replace(/<script.*<\/script>/gi, ''));
-        document.body.innerHTML = "";
+        const data = JSON.parse(document.body.innerText.replace(/<script.*<\/script>/gi, ''));
+        document.body.innerText = "";
         document.documentElement.removeAttribute('hidden');
         if (render && typeof render == 'function') {
             render(data);
